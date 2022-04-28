f224863c9557726899b2cf23abbe713b024655de
dreamcatcher1712 <98090037+dreamcatcher1712@users.noreply.github.com>
2022-04-28 10:55:28 +0530

    Source Code

---
+++ calc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    float num1,num2,result;
+    printf("Enter an operator: ");
+    scanf("%c", &op);
+    printf("Enter two numbers: ");
+    scanf("%f%f", &num1, &num2);
+    switch (op)
+    {
+        case '+':
+        result = num1+num2;
+        break;
+        case '-':
+        result = num1-num2;
+        break;
+        case '*':
+        result = num1*num2;
+        break;
+        case '/':
+        result = num1/num2;
+        break;
+        default: printf ("Enter valid choice");
+        break;
+    }
+    printf("%.2f is the result of %c on %.2f and %.2f" , result, op, num1, num2);
+    
+    return 0;}
